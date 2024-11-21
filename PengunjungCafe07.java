6392afe6358c57bb6b3f9f535b2ecda634d8f7d5
Dzaky-Hadyan <dzakyhadyaneliyanta06@gmail.com>
2024-11-21 08:27:37 +0700

    feat: modifying percobaan 4

--- PengunjungCafe07.java
+++ PengunjungCafe07.java
@@ -1,8 +1,8 @@
 public class PengunjungCafe07 {
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("-" + namaPengunjung[i]);
+        for (String string : namaPengunjung) {   
+            System.out.println("-" + string);
         }
     }
     public static void main(String[] args) {
